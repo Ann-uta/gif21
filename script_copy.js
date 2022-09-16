1857548f33edaf1e35fa12efeb25bc13a751de6e
Anna <momannuta@gmail.com>
2022-09-16 14:29:28 +0300

    1

--- script_copy.js
+++ script_copy.js
@@ -2,13 +2,14 @@ let userRequest = document.querySelector("#userRequest");
 
 async function onSearch() {
     let out = '';
-    const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=nLFa4mMSmxf18GK4EjbWzeX98rxcNQ1U&q=" + userRequest.value +"&limit=5&offset=0&rating=g&lang=en");
+    try {
+        const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=nLFa4mMSmxf18GK4EjbWzeX98rxcNQ1U&q=" + userRequest.value +"&limit=5&offset=0&rating=g&lang=en");
     const result = await response.json();
     for (let d of result.data){
         out += `<img src='${d.images.original.url}'><br>`;
         }        
     console.log(out);
-    try {
+    
         if (out == ''){
             throw new Error ("Ничего не найдено")
         }else {
